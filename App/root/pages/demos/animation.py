cbac29a72e83a166512aee6854fa7ad4ceea5f10
Alan G <72740158+N0va9@users.noreply.github.com>
2025-07-08 14:53:57 +0200

    Update animation.py

--- App/root/pages/demos/animation.py
+++ App/root/pages/demos/animation.py
@@ -42,6 +42,8 @@ for frame_num, a in enumerate(np.linspace(0.0, 4 * np.pi, 100)):
     # Update the image placeholder by calling the image() function on it.
     image.image(1.0 - (n_matrix / n_matrix.max()), use_container_width=True)
 
+    time.sleep(0.05) # Simulate a delay for the animation effect
+
 # We clear elements by calling empty on them.
 progress_bar.empty()
 frame_text.empty()
